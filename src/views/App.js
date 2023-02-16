416f35446c9ddce2810f0345bf39c5b155217f90
quang huy <quanghuy060392@gmail.com>
2023-02-16 15:29:41 +0700

    merge update app.js

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
-
+import MyComponent from './example/MyComponent';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. new commit
+          HuyIT123 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,8 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
+
           Learn React
         </a>
+        <MyComponent />
       </header>
     </div>
   );
