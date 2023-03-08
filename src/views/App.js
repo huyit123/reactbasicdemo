2faf1435c44ca669a38a20e8cae58d9d086bdeed
quang huy <quanghuy060392@gmail.com>
2023-03-08 21:25:36 +0700

    lan thu 7

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>src/App.js</code> and save to reload ewqeqweqweqweqewq
+          <code>src/App.js</code> and save to reload mai ben nhau ban nhes
+          
           001
         </p>
         <a
