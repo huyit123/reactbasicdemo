7fa91f0c90b1b2787a94c8c10d050640cbff9621
huyit123 <quanghuy060392@gmail.com>
2023-03-08 21:06:11 +0700

    Update App.js
    lankasd

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload ewqeqweqweqweqewq
           001
         </p>
         <a
